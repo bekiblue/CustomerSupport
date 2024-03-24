a06d57bb4b42395af2508fab47d2cc812d0599d7
Hailemari <hailemariamkefale19@gmail.com>
2024-03-24 15:47:55 +0300

    add requirements.txt

--- app.js
+++ app.js
@@ -1,19 +1,83 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
-const axios = require('axios');
 const bodyParser = require('body-parser');
-const app = express();
+const axios = require('axios');
+const { spawn } = require('child_process');
+
+const authenticateToken = require('./middleware/authMiddleware');
+
 require('dotenv').config();
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/api/customer-data', async (req, res) => {
+
+
+const uri = process.env.MONGODB_URI;
+
+async function connectToDB() {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB');
+}
+connectToDB();
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+app.post('/api/login', async (req, res) => {
+  let { email, password } = req.body;
+  console.log('email', email);
+  try {
+    const user = await User.findOne({ email });
+
+    console.log('user', user);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+  
+    const isValidPassword = password === user.password;
+
+    console.log('isValidPassword', isValidPassword);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+  
+    // Generate JWT
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    })
+    console.log('token', token);
+
+    // Successful login
+    return res.json({ token });
+
+  } catch (error) {
+    console.log('error', error);
+    console.error(error);
+    res.status(500).json({ message: 'Server error'});
+  }
+});
+
+
+app.get('/api/customer-data', authenticateToken, async (req, res) => {
   console.log('new request');
-  const { email } = req.body;
+  const { email } = req.query;
+  
 
   try {
     const response = await axios.get('https://api.gumroad.com/v2/sales', {
@@ -22,7 +86,7 @@ app.get('/api/customer-data', async (req, res) => {
         access_token: process.env.access_token,
       },
     });
-
+    console.log('Response', response.data);
     res.json(response.data);
   } catch (error) {
     console.error('Failed to fetch customer data.', error);
@@ -30,9 +94,10 @@ app.get('/api/customer-data', async (req, res) => {
   }
 });
 
-app.post('/api/execute-python-script', (req, res) => {
+app.post('/api/execute-python-script', authenticateToken, (req, res) => {
   console.log('new request');
   const { selectedProductID, licenseKey } = req.body;
+ 
 
   const pythonProcess = spawn('python3', [
     './script/script.py',
@@ -64,5 +129,5 @@ app.post('/api/execute-python-script', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port 'http://localhost:${PORT}'`);
+  console.log('Server is running');
 });
